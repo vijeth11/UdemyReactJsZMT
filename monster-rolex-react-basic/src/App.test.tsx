021c693e7145068acdc84c82d15c9d734cc0cfc9
Vijeth Belle <vijeth11@yahoo.co.in>
2023-03-04 22:37:04 +0530

    added test cases

--- monster-rolex-react-basic/src/App.test.tsx
+++ monster-rolex-react-basic/src/App.test.tsx
@@ -1,11 +1,34 @@
 import { shallow } from 'enzyme';
+import * as utils from './utils/data.utils';
 import App from './App';
+import { Monster } from './types/types';
+import { SearchInput } from './component/search-box/search-box.component';
+import { ChangeEvent, Component } from 'react';
+import { CardList } from './component/card-list/card-list.component';
+
+const mockData:Monster[] = [
+    {
+        email:"test@test.com",
+        id:"1",
+        name:"test"
+    },
+    {
+        email:"test1@test.com",
+        id:"2",
+        name:"test2"
+    }
+]
 
 it("initial test",() => {
     expect(shallow(<App/>)).toMatchSnapshot();
 })
 
 it('correctly filters the cards',() => {
-    const wrapper = shallow(<App/>);
-    
+    const mockSearchData = {target:{value:"test1"}} as ChangeEvent<HTMLInputElement>;
+    const spy = jest.spyOn(utils,'getData').mockResolvedValue(mockData);
+    const wrapper = shallow<Component<{},{searchField:string, monsters:Monster[]}>>(<App/>);
+    wrapper.find(SearchInput).prop('handleChange')(mockSearchData);
+    expect(spy).toHaveBeenCalled();
+    expect(wrapper.state().searchField).toBe("test1");
+    expect(wrapper.find(CardList).length).toBe(1);
 })
